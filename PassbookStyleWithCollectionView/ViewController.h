c62dff62776725691ae8d23a07fd2c306347a042
Xavier Roman <xavier.roman@gmail.com>
2013-04-08 18:23:03 +0800

    Initial Commit

---
+++ PassbookStyleWithCollectionView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PassbookStyleWithCollectionView
+//
+//  Created by Xavi on 8/4/13.
+//  Copyright (c) 2013 Xavier Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
