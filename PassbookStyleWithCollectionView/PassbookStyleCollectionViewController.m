8d1c1f6ea6eb5b67c9061c9848e6a6c051622dd8
Xavi <Xavi@Sherkan.local>
2013-04-18 01:23:03 +0800

    -Cleaning code and values definition

--- PassbookStyleWithCollectionView/PassbookStyleCollectionViewController.m
+++ PassbookStyleWithCollectionView/PassbookStyleCollectionViewController.m
@@ -11,78 +11,60 @@
 #import <QuartzCore/CAGradientLayer.h>
 #import <QuartzCore/CAShapeLayer.h>
 
+#define DEFAULT_CELL_HEIGHT 80
+#define SIZE_NOT_SELECTED_CELLS 8
+
 @interface PassbookStyleCollectionViewController ()
 
 @property (nonatomic,strong) NSMutableArray *backgroundColors;
-@property (nonatomic) BOOL couponSelected;
-@property (nonatomic) NSUInteger positionCouponSelected;
+@property (nonatomic) BOOL isCellSelected;
+@property (nonatomic) NSUInteger positionCellSelected;
+@property (nonatomic) BOOL isConstrainedSizeCellsToViewSize;
 
 @end
 
 @implementation PassbookStyleCollectionViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.couponSelected = false;
-    self.positionCouponSelected = -1;
-    self.backgroundColors = [[NSMutableArray alloc]initWithCapacity:6];
-    [self.backgroundColors addObject:[UIColor redColor]];
-    [self.backgroundColors addObject:[UIColor orangeColor]];
-    [self.backgroundColors addObject:[UIColor yellowColor]];
-    [self.backgroundColors addObject:[UIColor greenColor]];
-    [self.backgroundColors addObject:[UIColor blueColor]];
-    [self.backgroundColors addObject:[UIColor purpleColor]];
+    
+    self.isCellSelected = false;
+    self.isConstrainedSizeCellsToViewSize = false;
+    self.positionCellSelected = -1;
+    self.backgroundColors = [NSMutableArray arrayWithArray:@[[UIColor redColor],[UIColor orangeColor], [UIColor yellowColor], [UIColor greenColor], [UIColor blueColor], [UIColor purpleColor]]];
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+#pragma mark - Collection View Delegate
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    return 6;
+    return [self.backgroundColors count];
 }
 
-//- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section{
-//
-//    if((self.couponSelected) && (section ==0))
-//        return 10.0;
-//    else return 0.0;
-//
-//}
-//
-//- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section{
-//    if((self.couponSelected) && (section ==0))
-//        return 3.0;
-//    else return 0.0;
-//}
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    if(!self.couponSelected)
-        return CGSizeMake(320, 80);
+    if(!self.isCellSelected)
+        if(!self.isConstrainedSizeCellsToViewSize){
+            return CGSizeMake(self.view.bounds.size.width, DEFAULT_CELL_HEIGHT);
+        }
+        else{
+            return CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height/self.backgroundColors.count);
+        }
     else{
         if(indexPath.row == 0){
-            return CGSizeMake(320, 420);
+            return CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height - ((self.backgroundColors.count)*SIZE_NOT_SELECTED_CELLS));
         }
+        //This is to create the depth appearance for the not selected cells, tweak the default width factor(0.92) and the incremental value (4) as you wish 
         else if (indexPath.row < (self.backgroundColors.count -1))
-            return CGSizeMake(300+4*(indexPath.row), 5);
+            return CGSizeMake((0.92f*self.view.bounds.size.width)+4.0f*(indexPath.row), SIZE_NOT_SELECTED_CELLS);
+        //And the final cell (double than the others)
         else
-            return CGSizeMake(320, 40 - 5*(self.backgroundColors.count -2));
+            return CGSizeMake(self.view.bounds.size.width, 2*SIZE_NOT_SELECTED_CELLS);
     }
-    
+
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
@@ -90,43 +72,38 @@
     
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
     cell.backgroundColor = [self.backgroundColors objectAtIndex:indexPath.row];
-    if((!self.couponSelected)||(indexPath.row ==0)){
+    
+    if((!self.isCellSelected)||(indexPath.row ==0)){
         cell.layer.shadowColor = [[UIColor blackColor] CGColor];
         cell.layer.shadowOffset = CGSizeMake(0.0f, 0.0f);
         cell.layer.shadowRadius = 5.0f;
         cell.layer.shadowOpacity = 1.0f;
         
-        
-        //    CAShapeLayer * maskLayer = [CAShapeLayer layer];
-        //    maskLayer.path = [UIBezierPath bezierPathWithRoundedRect: cell.bounds byRoundingCorners: UIRectCornerTopLeft | UIRectCornerTopRight cornerRadii: (CGSize){10.0, 10.}].CGPath;
-        //
-        //    cell.layer.mask = maskLayer;
-        
         [cell.layer setShadowPath:[UIBezierPath bezierPathWithRect:cell.bounds].CGPath];
+        cell.clipsToBounds = NO;
     }
-    if(((indexPath.row == 0)&& (self.couponSelected))|| ((indexPath.row == self.backgroundColors.count -1)&& (self.couponSelected)))
+    if(((indexPath.row == 0) && (self.isCellSelected)) || ((indexPath.row == self.backgroundColors.count -1) && (self.isCellSelected)))
         cell.layer.cornerRadius = 5.0f;
     else
-        cell.layer.cornerRadius = 0.0f;
-    cell.clipsToBounds = NO;
+        cell.layer.cornerRadius = 1.0f;
+    
     return cell;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
     
-    if(self.couponSelected){
-        [self moveObjectFromIndex:0 toIndex:self.positionCouponSelected];
-        self.positionCouponSelected = -1;
+    if(self.isCellSelected){
+        [self moveObjectFromIndex:0 toIndex:self.positionCellSelected];
     }
     else{
-        self.positionCouponSelected = indexPath.row;
-        [self moveObjectFromIndex:self.positionCouponSelected toIndex:0];
+        self.positionCellSelected = indexPath.row;
+        [self moveObjectFromIndex:self.positionCellSelected toIndex:0];
     }
     [self.collectionView moveItemAtIndexPath:indexPath toIndexPath:[NSIndexPath indexPathForItem:0 inSection:indexPath.section]];
-    self.couponSelected = !self.couponSelected;
+    self.isCellSelected = !self.isCellSelected;
     [self.collectionView performBatchUpdates:^{
         [self.collectionView reloadSections:[NSIndexSet indexSetWithIndex:0]];
-    } completion:^(BOOL finished) {}];
+    } completion:nil];
     
 }
 
